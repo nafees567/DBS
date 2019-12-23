1bf0e5d1cfc8419293826b2c9572fba9cf15897b
M-Hammad-Ali <55716079+M-Hammad-Ali@users.noreply.github.com>
2019-12-23 14:07:45 +0500

    Connection, contact, doctor and queries added

---
+++ contact.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="logo.png">
+
+    <title>Contact Us</title>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle = "collapse" data-target = "#navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html"><img id="logo" src="logo.png" alt="Website Logo" width="40" height="40"></a>
+        </div>
+
+        <div class="collapse navbar-collapse navbar-right" id="navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="signIn.php">Sign in</a></li>
+                <li><a href="admin.php">Admin</a></li>
+                <li class="active"><a href="contact.php">Contact Us</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="center">
+    <h2 id="contactH2" class=" contactDiv">Contact Us</h2>
+<!--    <h3 id="contactH3">We are open 24/7</h3>-->
+    <br>
+    <br>
+    <br>
+    <p class="lead contactDiv">Email: help@hospital.com</p>
+    <p class="lead contactDiv">Phone: 051-1234567</p>
+    <p class="lead contactDiv">Address: ABC Road, Islamabad</p>
+</div>
+
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
